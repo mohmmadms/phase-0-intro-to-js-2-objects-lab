8585c9498e6bab2da3a39e18295ddca19c584c09
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-09-28 02:30:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    const updatedEmployee = { ...employee };
+   
+    updatedEmployee[key] = value;
+  
+    return updatedEmployee;
+  }
+  
+ 
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    employee[key] = value;
+   
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee, key) {
+   
+    const updatedEmployee = { ...employee };
+   
+    delete updatedEmployee[key];
+   
+    return updatedEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   
+    delete employee[key];
+   
+    return employee;
+  }
+  
+  const employee = {
+    name: 'John',
+    location: 'zarqa',
+    
+  };
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'location', 'amman');
+  console.log(updatedEmployee); 
+  
+  const destructivelyUpdatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'location', 'ajloun');
+  console.log(destructivelyUpdatedEmployee); 
+  const employeeWithoutAge = deleteFromEmployeeByKey(employee, 'age');
+  console.log(employeeWithoutAge); 
+  
+  const originalEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+  console.log(originalEmployee); 
+  
+  
